bd176bd62987ec38db1c1ffa1a29fa6a73b72ddc
vladyslav1243 <galich.vinfort@gmail.com>
2020-11-18 17:57:58 +0200

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var myWidth = window.innerWidth,
+    myHeight = window.innerHeight;
+console.log("width ".concat(myWidth, " \n height ").concat(myHeight));
+
+window.onload = function () {// /*
+  //     change href on mobile
+  //  */
+  //
+  // if(/iPhone|iPod|Android/i.test(navigator.userAgent)){
+  //     document.querySelector('a.grande').href = '#formgrande';
+  //     document.querySelector('a.lake').href = '#formlake';
+  //     document.querySelector('a.lou').href = '#formlou';
+  // }
+};
---
+++ app/script/main.js
@@ -0,0 +1,17 @@
+let myWidth = window.innerWidth,
+    myHeight = window.innerHeight;
+console.log(`width ${myWidth} \n height ${myHeight}`);
+
+window.onload = function () {
+
+
+    // /*
+    //     change href on mobile
+    //  */
+    //
+    // if(/iPhone|iPod|Android/i.test(navigator.userAgent)){
+    //     document.querySelector('a.grande').href = '#formgrande';
+    //     document.querySelector('a.lake').href = '#formlake';
+    //     document.querySelector('a.lou').href = '#formlou';
+    // }
+};
